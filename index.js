3faf2ab2c0a9d3d6bb1984930763cd09c8656972
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-06 00:32:12 +0700

    Add ffmpeg

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const NodeMediaServer = require('node-media-server');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 const config = {
   rtmp: {
