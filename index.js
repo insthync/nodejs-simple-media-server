7ea6ca023bf561ab3970cd1ff9a4c27e5e1d9186
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-06 15:59:19 +0700

    WIP media subscribing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const express = require('express');
+const fileupload = require("express-fileupload");
 const bodyParser = require('body-parser');
 const http = require('http');
 const morgan = require('morgan');
@@ -14,31 +15,23 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileupload());
 const server = http.createServer(app);
 const io = new Server(server);
 
 const playLists = {};
-
-// Before start the server, load playlist
-const videos = await prisma.videos.findMany({
-  orderBy: {
-    sortOrder: 'asc'
-  }
-});
-videos.forEach(video => {
-  if (!Object.hasOwnProperty.call(playLists, video.playListId)) {
-    playLists[video.playListId] = [];
-  }
-  playLists[video.playListId].push(video);
-});
+const playListSubscribers = {};
 
 io.on('connection', (socket) => {
-  socket.on('current', (msg) => {
+  socket.on('sub', (msg) => {
     const playListId = msg.playListId;
     const currentPlayList = playLists[playListId];
+    const currentPlayListSubscribers = playListSubscribers[playListId];
+    currentPlayListSubscribers.push(socket);
     // Response current media to the client
     socket.emit("resp", {
       playListId: playListId,
+      mediaId: currentPlayList.mediaId,
       isPlaying: currentPlayList.isPlaying,
       filePath: currentPlayList.filePath,
       position: currentPlayList.position,
@@ -48,58 +41,75 @@ io.on('connection', (socket) => {
   socket.on('play', (msg) => {
     const playListId = msg.playListId;
     const currentPlayList = playLists[playListId];
+    const currentPlayListSubscribers = playListSubscribers[playListId];
     currentPlayList.isPlaying = true;
     playLists[playListId] = currentPlayList;
-    socket.emit("resp", {
-      playListId: playListId,
-      isPlaying: currentPlayList.isPlaying,
-      filePath: currentPlayList.filePath,
-      position: currentPlayList.position,
+    currentPlayListSubscribers.forEach(element => {
+      element.emit("resp", {
+        playListId: playListId,
+        mediaId: currentPlayList.mediaId,
+        isPlaying: currentPlayList.isPlaying,
+        filePath: currentPlayList.filePath,
+        position: currentPlayList.position,
+      });
     });
   });
 
   socket.on('pause', (msg) => {
     const playListId = msg.playListId;
     const currentPlayList = playLists[playListId];
+    const currentPlayListSubscribers = playListSubscribers[playListId];
     currentPlayList.isPlaying = false;
     playLists[playListId] = currentPlayList;
-    socket.emit("resp", {
-      playListId: playListId,
-      isPlaying: currentPlayList.isPlaying,
-      filePath: currentPlayList.filePath,
-      position: currentPlayList.position,
+    currentPlayListSubscribers.forEach(element => {
+      element.emit("resp", {
+        playListId: playListId,
+        mediaId: currentPlayList.mediaId,
+        isPlaying: currentPlayList.isPlaying,
+        filePath: currentPlayList.filePath,
+        position: currentPlayList.position,
+      });
     });
   });
 
   socket.on('stop', (msg) => {
     const playListId = msg.playListId;
     const currentPlayList = playLists[playListId];
+    const currentPlayListSubscribers = playListSubscribers[playListId];
     currentPlayList.isPlaying = false;
     currentPlayList.position = 0;
     playLists[playListId] = currentPlayList;
-    socket.emit("resp", {
-      playListId: currentPlayList.playListId,
-      isPlaying: currentPlayList.isPlaying,
-      filePath: currentPlayList.filePath,
-      position: currentPlayList.position,
+    currentPlayListSubscribers.forEach(element => {
+      element.emit("resp", {
+        playListId: playListId,
+        mediaId: currentPlayList.mediaId,
+        isPlaying: currentPlayList.isPlaying,
+        filePath: currentPlayList.filePath,
+        position: currentPlayList.position,
+      });
     });
   });
 
   socket.on('seek', (msg) => {
     const playListId = msg.playListId;
     const currentPlayList = playLists[playListId];
+    const currentPlayListSubscribers = playListSubscribers[playListId];
     currentPlayList.position = msg.position;
     playLists[playListId] = currentPlayList;
-    socket.emit("resp", {
-      playListId: currentPlayList.playListId,
-      isPlaying: currentPlayList.isPlaying,
-      filePath: currentPlayList.filePath,
-      position: currentPlayList.position,
+    currentPlayListSubscribers.forEach(element => {
+      element.emit("resp", {
+        playListId: playListId,
+        mediaId: currentPlayList.mediaId,
+        isPlaying: currentPlayList.isPlaying,
+        filePath: currentPlayList.filePath,
+        position: currentPlayList.position,
+      });
     });
   });
 });
 
 function validateUser(req, res, next) {
+  /*
   // Validate connection by secret key which will be included in header -> authorization
   // TODO: Implements middleware if there are more than 1 function which will validate authorization like this
   const bearerHeader = req.headers['authorization']
@@ -114,6 +124,7 @@ function validateUser(req, res, next) {
     res.sendStatus(400)
     return;
   }
+  */
   next();
 };
 
@@ -124,18 +135,18 @@ app.post('/upload', validateUser, async (req, res, next) => {
       res.sendStatus(404);
     } else {
       // Get and move video file to upload path
+      const id = nanoid();
       const video = req.files.video;
       const fileName = video.name;
-      const savePath = './uploads/' + fileName;
+      const savePath = './uploads/' + id + '_' + fileName;
       video.mv(savePath);
 
       const duration = await getVideoDurationInSeconds(
         savePath
       );
 
-      const id = nanoid();
       // Store video to database
-      const newVideo = await prisma.videos.create({
+      await prisma.videos.create({
         data: {
           id: id,
           playListId: req.playListId,
@@ -143,11 +154,6 @@ app.post('/upload', validateUser, async (req, res, next) => {
           duration: duration,
         }
       });
-      // Store video to cache
-      if (!Object.hasOwnProperty.call(playLists, video.playListId)) {
-        playLists[video.playListId] = [];
-      }
-      playLists[video.playListId].push(newVideo);
 
       res.status(200).send();
     }
@@ -162,24 +168,19 @@ app.delete('/:id', validateUser, async (req, res, next) => {
       id: req.query.id,
     }
   });
-  // Find and delete from playlist
-  for (const playListId in playLists) {
-    if (!Object.hasOwnProperty.call(playLists, playListId)) {
-      continue;
-    }
-    const playList = playLists[playListId];
-    for (let i = playList.length - 1; i >= 0; --i) {
-      if (playList[i].id == req.query.id) {
-        playList.splice(i, 1);
-        break;
-      }
-    }
-  }
   res.status(200).send();
 });
 
 app.get('/:playListId', async (req, res) => {
-  res.status(200).send(playLists[req.query.playListId]);
+  const videos = await prisma.videos.findMany({
+    where: {
+      playListId: playListId,
+    },
+    orderBy: {
+      sortOrder: 'asc'
+    }
+  });
+  res.status(200).send(videos);
 });
 
 const port = Number(process.env.SERVER_PORT || 8216);
