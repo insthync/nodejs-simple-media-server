dc791bbe9ed42e001e5fc1def56df9c24c76a05d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-07 01:19:41 +0700

    Allow client to read files in uploads folder

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
-app.use(express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
 
 const server = http.createServer(app);
 const io = new Server(server);
